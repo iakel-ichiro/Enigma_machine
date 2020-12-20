3947b7e993289e31478fc169255393c7e157d0c0
iakel-ichiro <ichigo.ryukiv@gmail.com>
2020-12-20 23:27:35 +0000

    createdwq

---
+++ sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+  // Sets the screen to be 720 pixels wide and 400 pixels high
+  createCanvas(720, 400);
+  background(0);
+}
+
+function draw() {
+    fill(255,0,0)
+    ellipse(50, 50, 80, 80);
+}
