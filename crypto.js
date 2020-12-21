0dd86ea7a3d94dd5d4b2589143fd5b3d1898ac9f
iakel-ichiro <ichigo.ryukiv@gmail.com>
2020-12-21 13:18:41 +0000

    corrected the dial indexing for dial 3 in relation to input.length

--- crypto.js
+++ crypto.js
@@ -1,12 +1,13 @@
-var alph ="ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // Alphabet for initial encryption reference
+var alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // Alphabet for initial encryption reference
+var i_pos = [0, 0, 0]; // boolean to make sure only updates it once during a blank input
 // rotor dictionary contains three rotors:
-rotors = { // Three Rotors: Fast, med, slow. [scramble, rotation index]
-    "2": ["EKMFLGDQVZNTOWYHXUSPAIBRCJ", 0], //fast
-    "1": ["AJDKSIRUXBLHWTMCQGZNPYFVOE", 0], //med
-    "0": ["BDFHJLCPRTXVZNYEIWGAKMUSQO", 0] // slow
+rotors = { // Three Rotors: Fast, med, slow. [scramble, rotation index, turnover start, end, notched?]
+    "1": ["EKMFLGDQVZNTOWYHXUSPAIBRCJ", 0, 'Q', 'R', false], //fast
+    "2": ["AJDKSIRUXBLHWTMCQGZNPYFVOE", 0, 'E', 'F', false], //med
+    "3": ["BDFHJLCPRTXVZNYEIWGAKMUSQO", 0, 'V', 'W', false] // slow
 }
-order = [0, 1, 2]; // order that the rotors are placed in the machine
-
+//       L, M, R
+order = [1, 2, 3]; // Chosen Rotors and order in machine, left to right
 // Encryption function
 function rotor_encryptor(parse, r_n, arr = alph) {
     permutation = rotors[r_n][0]; // Access the scramble from specified rotor
@@ -27,7 +28,28 @@ function rotor_encryptor(parse, r_n, arr = alph) {
 // This function performs the encryption for all dials sequentially, subbing in the previous dial as the reference array after the first dial.
 //There is the possibility that it is not necassary to sub in reference arrays and leave them all as alphabet, but i think this is right.
 function encrypt() {
-    var x = rotor_encryptor(input.value(), 2);
-    var y = rotor_encryptor(x, 1, rotors[2][0]);
-    return rotor_encryptor(y, 0, rotors[1][0]);
+
+    var x = rotor_encryptor(input.value(), order[2]);
+    var y = rotor_encryptor(x, order[1], rotors[2][0]);
+    return rotor_encryptor(y, order[0], rotors[1][0]);
+}
+
+function rotate_dial() {
+    let l = input.value().length;
+    for (var i = 0; i <= l; i++) {
+        rotors[order[2]][1] = scroll_compute(i_pos[2], i);
+        // if (alph[rotors[order[2]][1]] == rotors[order[2]][3]) { // if rotor index = turnover end
+        //     if (rotors[order[2]][4]) { // check to see if it has been notched by turnover start
+        //         rotors[order[1]][1] = scroll_compute(rotors[order[1]][1], 1);
+        //
+        //     }
+        // }
+    }
+
+}
+
+function update_i_pos() {
+    i_pos[2] = rotors[order[2]][1];
+    i_pos[1] = rotors[order[1]][1];
+    i_pos[0] = rotors[order[0]][1];
 }
--- render.js
+++ render.js
@@ -13,10 +13,16 @@ function render_console() {
     textSize(16);
     textStyle(NORMAL);
     let x = "";
-    if (input.value() !== "") { // only perform encryption if text is entered
+    rotate_dial()
+    if (input.value() == "") { // only perform encryption if text is entered
+        update_i_pos();
+    } else {
         x = encrypt();
+
     }
+
     text("Console: " + x, 25, 190); // type out encryption into Console:
+    text("Tests: " +  input.value().length + "ipos" + i_pos[2], 25, 220); // type out encryption into Console:
     noFill();
     stroke(50);
     rect(width / 2, 220, width * 0.95, 100);
@@ -31,18 +37,24 @@ function user_input() {
 // Function to interact with dial indexing
 function mouseWheel(event) {
     if ((mouseX > dial1_x - 20) && (mouseX < dial1_x + 20) && (mouseY > dial_y - 40) && (mouseY < dial_y + 40)) {
-        rotors[order[0]][1] = scroll_compute(rotors[order[0]][1]);
+        rotors[order[0]][1] = scroll_compute(rotors[order[0]][1], event.delta);
+        update_i_pos();
     } else if ((mouseX > dial2_x - 20) && (mouseX < dial2_x + 20) && (mouseY > dial_y - 40) && (mouseY < dial_y + 40)) {
-        rotors[order[1]][1] = scroll_compute(rotors[order[1]][1]);
+        rotors[order[1]][1] = scroll_compute(rotors[order[1]][1], event.delta);
+        update_i_pos();
     } else if ((mouseX > dial3_x - 20) && (mouseX < dial3_x + 20) && (mouseY > dial_y - 40) && (mouseY < dial_y + 40)) {
-        rotors[order[2]][1] = scroll_compute(rotors[order[2]][1]);
+        rotors[order[2]][1] = scroll_compute(rotors[order[2]][1], event.delta);
+        update_i_pos();
     }
 }
 
-function scroll_compute(index) {
-    print(event.delta);
+function scroll_compute(index, increment) {
     //move the square according to the vertical scroll amount
-    index += event.delta / 100;
+    print(increment)
+    if (abs(increment) == 100) {
+        increment = increment / 100;
+    }
+    index += increment;
     if (index > 25) {
         index = 0;
     }
@@ -66,11 +78,11 @@ function render_dials() {
     noFill();
     rectMode(CENTER)
     rect(dial1_x, dial_y, 40, 80);
-    text(rotors[order[0]][1] + 1, dial1_x - 17.5, dial_y + 10);
+    text(alph[rotors[order[0]][1]], dial1_x - 10, dial_y + 10);
     rect(dial2_x, dial_y, 40, 75);
-    text(rotors[order[1]][1] + 1, dial2_x - 17.5, dial_y + 10);
+    text(alph[rotors[order[1]][1]], dial2_x - 10, dial_y + 10);
     rect(dial3_x, dial_y, 40, 75);
-    text(rotors[order[2]][1] + 1, dial3_x - 17.5, dial_y + 10);
+    text(alph[rotors[order[2]][1]], dial3_x - 10, dial_y + 10);
     textSize(20);
     text(order[0], dial1_x - 5, dial_y + 60);
     text(order[1], dial2_x - 5, dial_y + 60);
